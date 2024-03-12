fb1fe1edb3c77ddd0de9943a1980cfc3566b7ae6
samuelclinton <contato@samuelclinton.com>
2024-03-11 22:07:30 -0300

    Criação do projeto

---
+++ src/test/java/com/cloudinn/backend/CloudInnBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudinn.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudInnBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
