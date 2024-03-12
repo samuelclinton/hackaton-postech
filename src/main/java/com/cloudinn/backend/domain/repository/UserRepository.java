de998fba56da56804de5529233b2b35a7f092719
samuelclinton <contato@samuelclinton.com>
2024-03-11 22:48:27 -0300

    Criação dos repositórios de user e role

---
+++ src/main/java/com/cloudinn/backend/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.cloudinn.backend.domain.repository;
+
+import com.cloudinn.backend.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
